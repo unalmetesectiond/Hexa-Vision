e5e5b6669b90739f0570eee82dfdb294825c03c2
Jiiy69 <loujay.liwanag@gmail.com>
2024-11-20 15:09:52 -0500

    update 2
    overall layout and design

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function closeSidebar() {
   document.getElementById("sidebar").style.width = "0";
 }
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('appointment-button').addEventListener('click', () => {
+      document.getElementById('appointment-form').classList.add('active');
+      document.getElementById('appointment-form').classList.remove('hidden');
+      document.getElementById('assistance-form').classList.add('hidden');
+      document.getElementById('assistance-form').classList.remove('active');
+  });
+
+  document.getElementById('assistance-button').addEventListener('click', () => {
+      document.getElementById('assistance-form').classList.add('active');
+      document.getElementById('assistance-form').classList.remove('hidden');
+      document.getElementById('appointment-form').classList.add('hidden');
+      document.getElementById('appointment-form').classList.remove('active');
+  });
+});
+
+document.querySelectorAll('.form-nav-buttons .nav-button').forEach(button => {
+  button.addEventListener('click', () => {
+      document.querySelectorAll('.form-nav-buttons .nav-button').forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+  });
+});
+
